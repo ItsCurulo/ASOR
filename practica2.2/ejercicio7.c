60dfc11a872dff17fe3aa4bf42ef6834ef4cb85f
Jaime Martinez Gamero <71594504+Jackesgamero@users.noreply.github.com>
2022-11-21 15:01:35 +0100

    Add files via upload

---
+++ practica2.2/ejercicio7.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+	if(argc==1){
+		printf("WARNING:filename missing\n");
+		exit(1);
+	}
+	int fd;
+	umask(024);
+	if(fd = open(argv[1], O_CREAT, 0645) == -1){
+		perror("Open");
+		exit(1);
+	}
+	return 0;
+}
